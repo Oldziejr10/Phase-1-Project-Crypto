8d7d8d4ffa3a394b5c532827cd49cf78555e9a06
Rafal Oldziej <rafal.oldziej1@gmail.com>
2022-07-11 23:14:38 -0400

    priceindex

--- node.js
+++ node.js
@@ -2,3 +2,6 @@ const bitcoinBtn = document.querySelector(".bitcoin");
 const ethBtn = document.querySelector(".eth");
 const rippleBtn = document.querySelector(".ripple")
 const binanceUrl = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd";
+const historyUrl = document.querySelector(".url")
+let priceIndex = document.querySelector(".add-crpyto-price")
+let cryptoArray = [];
